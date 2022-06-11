97962960a6da4aa7df6da6d552d84a1c6f605527
Pavel Mezencev <mezencev24@mail.ru>
2022-06-12 00:45:09 +0300

    add tabs functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//////////// SLIDER
 const slider = tns({
   container: '.carousel__inner',
   items: 1,
@@ -22,3 +23,29 @@ document.getElementById('prev').addEventListener('click', () => {
 document.getElementById('next').addEventListener('click', () => {
   slider.goTo('next');
 });
+
+//////////// TABS
+function toggleTabs(tabSelector, contentSelector) {
+  const tabs = document.querySelectorAll(tabSelector);
+  const contents = document.querySelectorAll(contentSelector);
+
+  function removeActiveClass() {
+    tabs.forEach((item) => {
+      item.classList.remove(`${tabSelector.replace(/\./g, '')}-active`);
+    });
+    contents.forEach((item) => {
+      item.classList.remove(`${contentSelector.replace(/\./g, '')}-active`);
+    });
+  }
+
+  tabs.forEach((item, i) => {
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      removeActiveClass();
+      tabs[i].classList.add(`${tabSelector.replace(/\./g, '')}-active`);
+      contents[i].classList.add(`${contentSelector.replace(/\./g, '')}-active`);
+    });
+  });
+}
+
+toggleTabs('.catalog__tab', '.catalog__container');
