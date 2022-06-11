e2573d60ccba2a3ac666bf0161bf28c73d4918e6
Pavel Mezencev <mezencev24@mail.ru>
2022-06-12 01:26:15 +0300

    fix catalog item animation

--- src/js/script.js
+++ src/js/script.js
@@ -40,7 +40,6 @@ function toggleTabs(tabSelector, contentSelector) {
 
   tabs.forEach((item, i) => {
     item.addEventListener('click', (e) => {
-      e.preventDefault();
       removeActiveClass();
       tabs[i].classList.add(`${tabSelector.replace(/\./g, '')}-active`);
       contents[i].classList.add(`${contentSelector.replace(/\./g, '')}-active`);
@@ -48,4 +47,36 @@ function toggleTabs(tabSelector, contentSelector) {
   });
 }
 
+function toggleItem(frontSelector, backSelector) {
+  const frontBtns = document.querySelectorAll(frontSelector);
+  const backBtns = document.querySelectorAll(backSelector);
+
+  function toggleSide(e) {
+    e.preventDefault();
+    const { target } = e;
+
+    if (target.classList.contains('catalog-item__link')) {
+      target.parentNode.classList.remove('catalog-item__body-active');
+      target
+        .closest('.catalog-item')
+        .querySelector('.catalog-item__list')
+        .classList.add('catalog-item__list-active');
+    } else {
+      target.parentNode.classList.remove('catalog-item__list-active');
+      target
+        .closest('.catalog-item')
+        .querySelector('.catalog-item__body')
+        .classList.add('catalog-item__body-active');
+    }
+  }
+
+  frontBtns.forEach((item) => {
+    item.addEventListener('click', toggleSide);
+  });
+  backBtns.forEach((item) => {
+    item.addEventListener('click', toggleSide);
+  });
+}
+
 toggleTabs('.catalog__tab', '.catalog__container');
+toggleItem('.catalog-item__link', '.catalog-item__back');
